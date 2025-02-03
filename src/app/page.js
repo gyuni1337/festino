038c841334f89e0248c60d0360f1609c9ca75805
gyuni1337 <silviplosakov@gmail.com>
2025-02-03 12:03:46 +0100

    creating easier path finding during development

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
-import Navbar from "./components/Navbar";
+import Navbar from "@/components/Navbar";
+
 
 export default function Home() {
 
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <main>
+
+      </main>
+    </>
   );
 }
