6a743936f218d05d8d1a187d6f77e87e6a9bc0b1
gyuni1337 <silviplosakov@gmail.com>
2025-03-23 17:22:38 +0100

    playing with icons

--- src/app/components/Map/MapComponent.js
+++ src/app/components/Map/MapComponent.js
@@ -2,7 +2,7 @@
 
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { Icon } from "leaflet";
+import { divIcon, Icon } from "leaflet";
 
 import MarkerComponent from "@/components/Map/MarkerComponent";
 
@@ -12,12 +12,26 @@ const customIcon = new Icon({
   iconSize: [80, 80],
 });
 
+const createCustomIcon = (title) =>
+  L.divIcon({
+    className: "custom-marker",
+    html: `<div style="text-align: center;">
+              <h1 style="color: red; font-size: 16px; font-weight: bold; margin-bottom: 5px;">${title}</h1>
+              <img 
+           </div>`,
+    iconSize: [30, 42], // Adjust size if necessary
+    iconAnchor: [15, 42], // Align correctly
+  });
 
 export default function MapComponent() {
   return (
     <MapContainer center={[56.043435668784134, 12.695556265645147]} zoom={15} className="absolute top-0 left-0 h-full z-0 w-full">
       <TileLayer url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png" />
-        <MarkerComponent key={"x"} position={[56.04434616837272, 12.694410151848887]} icon={customIcon}  />
+        {/* how to make this marker have onclick */}
+        <MarkerComponent key={"x"} position={[56.04434616837272, 12.694410151848887]}   onClick={() => console.log("Marker clicked!")}  icon={customIcon}  />
+    
+          
     </MapContainer>
+    
   );
 }
--- src/app/components/Map/MarkerComponent.js
+++ src/app/components/Map/MarkerComponent.js
@@ -3,12 +3,22 @@ import { Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 
 
-export default function MarkerComponent({key, position, icon, popup}) {
+const createCustomIcon = (title) =>
+  L.divIcon({
+    html: `<div style="display: flex; flex-direction: column; align-items: center; background: none;">
+              <h1 style="color: red; font-size: 16px; background: none; font-weight: bold; margin-bottom: 5px;">${title}</h1>
+              <img src="/lightMarker.png" />
+           </div>`,
+    iconSize: [30, 42], // Adjust size if necessary
+    iconAnchor: [15, 42], // Align correctly
+  });
+
+export default function MarkerComponent({key, position, iconImage, onClick }) {
   return (
-    <Marker key={key} position={position} icon={icon}>
-      <Popup>
-        {popup}
-        </Popup>
+    <div className='flex flex-col items-center'>
+       <h1>title of the thing</h1> 
+    <Marker key={key} position={position} icon={createCustomIcon("asd")} eventHandlers={{ click: onClick }}>
     </Marker>
+    </div>
   )
 }
