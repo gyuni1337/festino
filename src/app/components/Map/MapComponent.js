28d8be824d4920bfa49aea1e94d4e6222fc5d688
gyuni1337 <silviplosakov@gmail.com>
2025-03-27 12:25:33 +0100

    changed title

--- src/app/components/Map/MapComponent.js
+++ src/app/components/Map/MapComponent.js
@@ -8,8 +8,12 @@ import { divIcon, Icon } from "leaflet";
 import MarkerComponent from "@/components/Map/MarkerComponent";
 export default function MapComponent() {
 
+  const [markers, setMarkers] = useState([]);
+  const [selectedMarker, setSelectedMarker] = useState(null);
+
+
   useEffect(() => {
-    
+
 
     
   }, []); 
@@ -18,7 +22,7 @@ export default function MapComponent() {
     <MapContainer center={[56.043435668784134, 12.695556265645147]} zoom={15} className="absolute top-0 left-0 h-full z-0 w-full">
       <TileLayer url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png" />
         {/* how to make this marker have onclick */}
-        <MarkerComponent key={"x"} position={[56.04434616837272, 12.694410151848887]} />
+        <MarkerComponent key={"x"} title="Helsingborg C" position={[56.04434616837272, 12.694410151848887]} />
     
           
     </MapContainer>
